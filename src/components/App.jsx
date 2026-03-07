9295feb5a01e43723ab92329799425a22cf287d7
woitekp <wojtek.podbielski.programmer@gmail.com>
2026-03-07 17:07:47 +0100

    feat: add footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div><Header /></div>
+    <div>
+      <Header />
+      <Footer />  
+    </div>
   );
 }
 
