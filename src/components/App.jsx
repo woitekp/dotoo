5eba6151be2c300067cff50d0820aefa721fd3e4
woitekp <wojtek.podbielski.programmer@gmail.com>
2026-08-05 00:10:48 +0200

    feat(task): implement dynamic task list and add task functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 import Task from "./Task/Task";
-
-const tasks = [
-  { id: 1, title: "Learn JavaScript", content: "Practice arrays and objects" },
-  { id: 2, title: "Go for a walk", content: "30 minutes" },
-  { id: 3, title: "Learn React", content: "Today: useState" },
-];
+import CreateTask from "./Task/CreateTask";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(newTask) {
+    setTasks(prevTasks => {
+      return [...prevTasks, newTask];
+    });
+  }
+
   return (
     <div className="app">
       <Header />
 
+      <CreateTask onAdd={addTask} />
+
       <div className="main">
-        {tasks.map((task) => (
-          <Task key={task.id} title={task.title} content={task.content} />
+        {tasks.map((task, index) => (
+          <Task key={index} title={task.title} content={task.content} />
         ))}
       </div>
 
