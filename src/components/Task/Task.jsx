dd947ecf0fac636d37c2a7fd89ac4b940cfb459f
woitekp <wojtek.podbielski.programmer@gmail.com>
2026-08-06 00:08:49 +0200

    feat: add done and urgent task buttons

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,18 +1,49 @@
+import CheckIcon from "@mui/icons-material/Check";
 import DeleteIcon from "@mui/icons-material/Delete";
-
+import { useState } from "react";
 
 function Task(props) {
-    function handleClick() {
+    const [isImportant, setIsImportant] = useState(false);
+    const [isDone, setIsDone] = useState(false);
+
+    function toggleImportant() {
+        setIsImportant(!isImportant);
+    }
+
+    function toggleDone() {
+        setIsDone(!isDone);
+    }
+
+    function handleDelete() {
         props.onDelete(props.id);
     }
     
     return (
-        <div className="task">
+        <div className={`task ${isImportant ? "task-important" : ""} ${isDone ? "task-done" : ""}`}>
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button onClick={handleClick}>
-                <DeleteIcon />
-            </button>
+
+            <div className="task-actions">
+                <button
+                    className={`done-button ${isDone ? "done-active" : ""}`}
+                    onClick={toggleDone}
+                >
+                    <CheckIcon />
+                </button>
+
+
+                <button
+                    className={`important-button ${isImportant ? "important-active" : ""}`}
+                    onClick={toggleImportant}
+                >
+                    !
+                </button>
+
+                <button onClick={handleDelete}>
+                    <DeleteIcon />
+                </button>
+            </div>
+
         </div>
     );
 }
