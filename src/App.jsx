1c7001198f8270e5fe63746cef4b86dc308f7d3c
woitekp <wojtek.podbielski.programmer@gmail.com>
2026-09-05 23:40:37 +0200

    feat(routes): render pages through react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "@/pages/MainPage";
 import LoginPage from "@/pages/LoginPage";
 import useAuth from "@/context/auth/useAuth";
@@ -5,7 +6,15 @@ import useAuth from "@/context/auth/useAuth";
 function App() {
   const { user } = useAuth();
 
-  return user ? <MainPage /> : <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={user ? <MainPage /> : <Navigate to="/login" replace />} />
+        <Route path="/login" element={user ? <Navigate to="/" replace /> : <LoginPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
